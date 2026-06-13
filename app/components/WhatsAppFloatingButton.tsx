6dfe88cfe363efee150bbce7e11eccb0a8b13bf5
McLovin18 <hwcobena@espol.edu.ec>
2026-06-13 18:37:09 -0500

    done

--- app/components/WhatsAppFloatingButton.tsx
+++ app/components/WhatsAppFloatingButton.tsx
@@ -109,7 +109,7 @@ const WhatsAppFloatingButton: React.FC = () => {
       `}</style>
 
       <a
-        href="https://wa.me/593999999999"
+        href="https://wa.me/593967760599"
         target="_blank"
         rel="noreferrer"
         aria-label="WhatsApp Marca Estilo"
